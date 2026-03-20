3562fe03329452a156721298c8c8b9f6babe9d9b
Vinisha-725 <vinisha.sahoo@gmail.com>
2026-03-20 14:05:27 +0530

    UC1 Implementation

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,7 @@
 public class TrainConsistManagementApp {
+    public static void main(String[] args){
+        System.out.println("Welcome Train Consist Management System");
+        System.out.println("System Initialized.");
+
+    }
 }
